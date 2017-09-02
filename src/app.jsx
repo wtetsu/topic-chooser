be118ea54bc7bf94c068db71419b7103c0c9ee66
wtetsu <tewatana@gmail.com>
2017-09-02 15:56:58 +0900

    Polish

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ require('es6-promise').polyfill();
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import utils from './utils';
 
 class TopicChooser extends React.Component {
     constructor(props) {
@@ -9,8 +10,8 @@ class TopicChooser extends React.Component {
         this.state = {
             list: [
             ],
-            inputText: "",
-            selectedTopic: {}
+            selectedTopic: {},
+            buttonClassName: 'button-large swingimage'
         };
 
         axios.get('./data.json').then((response) => {
@@ -27,20 +28,16 @@ class TopicChooser extends React.Component {
                 <h1>Topic Chooser</h1>
                 <img src="logo.png" width="256" height="256" />
                 <p>Choose one topic randomly from <a target="_blank" href="http://iteslj.org/questions/">http://iteslj.org/questions/</a></p>
-                <p><button onClick={this.buttonPushed.bind(this)} className="button-large">PUSH TO SELECT!</button></p>
+                <p><button className={this.state.buttonClassName} onClick={this.buttonPushed.bind(this)}>PUSH TO SELECT!</button></p>
                 <p className={this.state.selectedTopic.className}><a target="_blank" href={'http://iteslj.org/questions/' + this.state.selectedTopic.link}>{this.state.selectedTopic.title}</a></p>
             </div>
         );
     }
     buttonPushed(e) {
-        function getRandomIntInclusive(min, max) {
-            min = Math.ceil(min);
-            max = Math.floor(max);
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
+        this.state.buttonClassName = "button-large";
         let selectedTopic = null;
         let id = setInterval(()=>{
-            let index = getRandomIntInclusive(0, this.state.list.length-1);
+            let index = utils.getRandomValue(0, this.state.list.length-1);
             selectedTopic = this.state.list[index];
             selectedTopic.className = null;
             this.state.selectedTopic = selectedTopic;
@@ -48,7 +45,7 @@ class TopicChooser extends React.Component {
         }, 10);
         setTimeout(()=>{
             clearInterval(id);
-            selectedTopic.className = "selected-topic"
+            selectedTopic.className = 'selected-topic';
             this.setState(this.state);
         }, 1500)
     }
@@ -71,4 +68,3 @@ ReactDOM.render(
   <App />,
   document.getElementById("root")
 );
-
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+let utils = {
+    getRandomValue(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+};
+
+export default utils;
