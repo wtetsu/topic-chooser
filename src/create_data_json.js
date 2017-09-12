fa775c393448b26a66333740d94cc2ba7127953c
wtetsu <tewatana@gmail.com>
2017-09-12 13:29:27 +0900

    Clean

--- src/create_data_json.js
+++ src/create_data_json.js
@@ -1,51 +1,52 @@
-let http = require('http');
-let fs = require('fs');
-let readline = require('readline');
+import http from 'http';
+import fs from 'fs';
+import readline from 'readline';
+
 const TMP_HTML_FILE = 'html.tmp';
 
 //const url = 'http://iteslj.org/questions/';
 const url = 'http://localhost/a.html';
 
 async function downloadAndSaveHtml() {
-  return new Promise((resolve)=>{
-    let file = fs.createWriteStream(TMP_HTML_FILE);
-    let request = http.get(url, (response)=>{
-        response.pipe(file);
-        resolve();
+    return new Promise((resolve)=>{
+        let file = fs.createWriteStream(TMP_HTML_FILE);
+        http.get(url, (response)=>{
+            response.pipe(file);
+            resolve();
+        });
     });
-  });
 }
 
 async function parseHtmlFile() {
-  let data = [];
-  return new Promise((resolve)=>{
-    let lineReader = readline.createInterface({
-        input: fs.createReadStream(TMP_HTML_FILE),
-    });
+    let data = [];
+    return new Promise((resolve)=>{
+        let lineReader = readline.createInterface({
+            input: fs.createReadStream(TMP_HTML_FILE),
+        });
 
-    let re = /\<a href=['"]([\w.]*)['"]\>([ :&;\.\?,'\/\-\w]+)\<\/a\>/g;
-    lineReader.on('line', (line)=>{
-        for (;;) {
-            let m = re.exec(line);
-            if (m === null) {
-              break;
-            }
-            let link = m[1];
-            let title = m[2].replace(/&amp;/g, "&");
-            data.push({link, title});
-        }        
+        let re = /<a href=['"]([\w.]*)['"]>([ :&;.?,'/\-\w]+)<\/a>/g;
+        lineReader.on('line', (line)=>{
+            for (;;) {
+                let m = re.exec(line);
+                if (m === null) {
+                    break;
+                }
+                let link = m[1];
+                let title = m[2].replace(/&amp;/g, '&');
+                data.push({link, title});
+            }        
+        });
+        lineReader.on('close', ()=>{
+            resolve(data);
+        });
     });
-    lineReader.on('close', (line)=>{
-        resolve(data);
-    });
-  });
 }
 
 function createDataJson(data) {
-  let json = "[\n" +
-             data.map((d)=>`{"link":"${d.link}","title":"${d.title}"}`).join(",\n") +
-             "\n]\n";
-  return json;
+    let json = '[\n' +
+               data.map((d)=>`{"link":"${d.link}","title":"${d.title}"}`).join(',\n') +
+               '\n]\n';
+    return json;
 }
 
 async function main() {
