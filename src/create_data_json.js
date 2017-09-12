f1dec10042e15a15725b40fcff33bf4f60f55a4c
wtetsu <tewatana@gmail.com>
2017-09-12 13:15:24 +0900

    Add a data creation tool

---
+++ src/create_data_json.js
@@ -0,0 +1,62 @@
+let http = require('http');
+let fs = require('fs');
+let readline = require('readline');
+const TMP_HTML_FILE = 'html.tmp';
+
+//const url = 'http://iteslj.org/questions/';
+const url = 'http://localhost/a.html';
+
+async function downloadAndSaveHtml() {
+  return new Promise((resolve)=>{
+    let file = fs.createWriteStream(TMP_HTML_FILE);
+    let request = http.get(url, (response)=>{
+        response.pipe(file);
+        resolve();
+    });
+  });
+}
+
+async function parseHtmlFile() {
+  let data = [];
+  return new Promise((resolve)=>{
+    let lineReader = readline.createInterface({
+        input: fs.createReadStream(TMP_HTML_FILE),
+    });
+
+    let re = /\<a href=['"]([\w.]*)['"]\>([ :&;\.\?,'\/\-\w]+)\<\/a\>/g;
+    lineReader.on('line', (line)=>{
+        for (;;) {
+            let m = re.exec(line);
+            if (m === null) {
+              break;
+            }
+            let link = m[1];
+            let title = m[2].replace(/&amp;/g, "&");
+            data.push({link, title});
+        }        
+    });
+    lineReader.on('close', (line)=>{
+        resolve(data);
+    });
+  });
+}
+
+function createDataJson(data) {
+  let json = "[\n" +
+             data.map((d)=>`{"link":"${d.link}","title":"${d.title}"}`).join(",\n") +
+             "\n]\n";
+  return json;
+}
+
+async function main() {
+    await downloadAndSaveHtml();
+    let data = await parseHtmlFile();
+    let json = createDataJson(data);
+    let stream = fs.createWriteStream('public/data.json');
+    stream.write(json);
+    stream.end();
+    fs.unlinkSync(TMP_HTML_FILE);
+}
+
+main();
+
