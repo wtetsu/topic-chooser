e0114992b55330d0ea01e60abaab89388222278c
wtetsu <tewatana@gmail.com>
2019-01-16 23:38:30 +0900

    Refresh packages and configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,55 @@
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const path = require('path');
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 const BASE_PLUGINS = [
-    new CopyWebpackPlugin([
-        { from: 'static', to: '.' },
-        { from: __dirname + '/node_modules/milligram/dist/milligram.min.css', to: '.' }
-    ]),
+  new CopyWebpackPlugin([
+    { from: "static", to: "." },
+    { from: __dirname + "/node_modules/milligram/dist/milligram.min.css", to: "." }
+  ])
 ];
 
 module.exports = {
-    context: path.join(__dirname, 'src'),
-    entry: './app.jsx',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: './app.js'
-    },
-    module: {
-        loaders: [
-        {
-            test: /\.js(x?)$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
+  context: path.join(__dirname, "src"),
+  entry: "./app.jsx",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "./app.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader"
         }
-      ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    plugins: (process.env.NODE_ENV === 'production')
-    ? BASE_PLUGINS.concat([
-        new UglifyJsPlugin({
-            minimize: true,
-            sourceMap: false,
-            compressor: {
-                warnings: false
-            },
-            output: {
-                comments: false
+      },
+      {
+        test: /\.jsx$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: { presets: ["@babel/env", "@babel/react"] }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  plugins:
+    process.env.NODE_ENV === "production"
+      ? BASE_PLUGINS.concat([
+          new webpack.DefinePlugin({
+            "process.env": {
+              NODE_ENV: JSON.stringify("production")
             }
-        }),
-        new webpack.DefinePlugin({
-            'process.env':{
-                'NODE_ENV': JSON.stringify('production')
-            }
-        })
-    ])
-    : BASE_PLUGINS,
-    devServer: {
-        contentBase: 'public'
-    },
-    devtool: (process.env.NODE_ENV === 'production')
-    ? 'source-map'
-    : ''
+          })
+        ])
+      : BASE_PLUGINS,
+  devServer: {
+    contentBase: "public"
+  },
+  devtool: process.env.NODE_ENV === "production" ? "source-map" : ""
 };
